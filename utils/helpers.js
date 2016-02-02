5c5665e908be1841f9cfb31eed61a65691e30779
scstan <silviu.stan90@gmail.com>
2016-02-02 23:20:09 +0200

    More work on setupdb + small refactoring

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,11 +3,11 @@ const http    = require('./httpStatusCodes')
 
 class utils {
   static sendResponse(result, res) {
-		return res.status(http.OK).send(result)
-	}
+    return res.status(http.OK).send(result)
+  }
   static sendErr(err, res){
     res.status(http.INTERNAL_SERVER_ERROR).send()
-    return console.error(err)
+    return console.log(err)
   }
 }
 
